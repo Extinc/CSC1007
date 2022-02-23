aa32e8aeac1a60e857a4ac576e1144689c497a02
bolo56moto <bolo56moto1@gmail.com>
2022-02-24 07:50:27 +0800

    Test

--- PageReplacementApp/FIFOPageReplacement.c
+++ PageReplacementApp/FIFOPageReplacement.c
@@ -23,17 +23,19 @@ void FIFOPageReplacement(LinkedList *data, int fsize) {
                 insert(memframe, pageval);
                 printf("STEP %d :\t", fault_count + 1);
                 printStep(memframe, fsize);
+                printf("\nno val and space\n");
             }else{
                 // WHen frames have been filled up to maximum
                 delete(memframe,0);
-
+                replace(memframe, pageval,getIndex(memframe, -1));
                 printf("STEP %d :\t", fault_count + 1);
-                insertAt(memframe,size-1,pageval);
                 printStep(memframe, fsize);
+                printf("\nno val no space\n");
             }
             fault_count++;
         }else{
             // If value exist in memory
+            printf("\nhave val\n");
         }
         i++;
         temp = temp->next;
