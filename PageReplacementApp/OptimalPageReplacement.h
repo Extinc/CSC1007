4884451937a7dd3bc463fbd3173ec4e976e78251
izzdanial <41255403+izzdanial99@users.noreply.github.com>
2022-03-04 22:57:23 +0800

    Update OptimalPageReplacement.h

--- PageReplacementApp/OptimalPageReplacement.h
+++ PageReplacementApp/OptimalPageReplacement.h
@@ -4,5 +4,7 @@
 
 #ifndef OSPROJ_OPTIMALPAGEREPLACEMENT_H
 #define OSPROJ_OPTIMALPAGEREPLACEMENT_H
+#include "LinkedList.h"
+void OptimalPageReplacement(LinkedList *data, int no_of_frames);
 
 #endif //OSPROJ_OPTIMALPAGEREPLACEMENT_H
